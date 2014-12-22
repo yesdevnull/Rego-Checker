78a40eeef3a509d68d3e94da32a7d61c35f8f1ee
Dan Barrett <dan@theblahman.net>
2014-12-22 21:27:49 +0800

    Initial commit

---
+++ app/controllers/RegoCheck.php
@@ -0,0 +1,79 @@
+<?php
+
+use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class RegoCheck extends Controller {
+	protected $webClient;
+
+	protected $apiClient;
+
+	public function __construct() {
+		$this->webClient = new Client();
+
+		$this->apiClient = new Client();
+	}
+
+	public function stateSwitch($state, $plate) {
+		switch ($state) {
+			case 'wa' :
+				return $this->waRegoCheck($plate);
+			break;
+		}
+	}
+
+	public function waRegoCheck($plate) {
+		$sessionRequest = $this->webClient->createRequest('GET', 'https://online.transport.wa.gov.au/webExternal/registration/?');
+
+		$sessionResponse = $this->webClient->send($sessionRequest);
+
+		$sessionBody = $sessionResponse->getBody();
+
+		$sessionSite = new Crawler($sessionBody->getContents());
+
+		$sessionId = substr($sessionSite->filter('div.licensing-big-form form')->attr('action'), 1);
+
+		if (!preg_match('#jsessionid#', $sessionId)) {
+			throw new Exception('Did not receive valid session');
+		}
+
+		$apiResponse = '';
+
+		try {
+			$apiResponse = $this->apiClient->post('https://online.transport.wa.gov.au/webExternal/registration' . $sessionId, [
+				'query' => ['plate' => $plate]
+			]);
+		} catch (RequestException $e) {
+			if ($e->hasResponse()) {
+				echo $e->getResponse() . "\n";
+			}
+		}
+
+		//dd($apiResponse->getBody());
+
+		if (is_object($apiResponse)) {
+			$apiResponseBody = $apiResponse->getBody();
+
+			$apiExpiryBody = new Crawler($apiResponseBody->getContents());
+
+			$expiryResults = $apiExpiryBody->filter('div.licensing-big-form .data span')->eq(1);
+
+			if (count($expiryResults) == 0) {
+				if ($apiExpiryBody->filter('.section-body p strong span')->first()->text()) {
+					throw new Exception('Unable to locate plate: ' . $plate);
+				} else {
+					throw new Exception('An unknown error occured');
+				}
+			} else {
+				$expiryResults = $expiryResults->text();
+			}
+
+			if (!preg_match("/[0-3][0-9]\/[0-1][0-9]\/[1-2][0-9]{3}/i", $expiryResults)) {
+				throw new Exception('Invalid date returned');
+			}
+
+			return $expiryResults;
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Rego Checker
+
+This tool allows you to check the current status of your vehicles registration in Western Australia.
+
+## Birth
+
+This idea came to light when I missed a registration from the Department of Transport (WA) with my registration renewal.  Since DoT no longer issue registration stickers, I wasn't aware of the expiry.  Needless to say, my car was unregistered for a few days which could have caused me grief, except for the fact I rarely drive so it wasn't a problem.
+
+I'm currently not aware of the DoT having a notification service, so I'm making one instead.
+
+## Plans
+
+I plan to get this working for all states of Australia that offer an online tool to check your registration.
