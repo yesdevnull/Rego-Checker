5aa27a9c1dcc567bb234639f55781e7098333101
Dan Barrett <dan@theblahman.net>
2015-02-19 08:52:07 +0800

    Broke out form and handler into two separate classes

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -6,23 +6,19 @@ require(['react', 'jquery'], function (React) {
         'use strict';
 
         var PlatesApp = React.createClass({
-            handlePlateSearch: function (e) {
-                e.preventDefault();
-
-                var plate = this.refs.plate.getDOMNode().value.trim();
-
+            handlePlateSearch: function (plate) {
                 $.ajax({
                     url: apiRoot + 'plate',
                     dataType: 'json',
                     type: 'POST',
-                    data: { plate: plate },
+                    data: plate ,
                     success: function(data) {
                         console.log(data);
                     }.bind(this),
                     error: function(xhr, status, err) {
                         console.error(this.props.url, status, err.toString());
                     }.bind(this)
-                })
+                });
             },
             render: function () {
                 return (
@@ -30,12 +26,32 @@ require(['react', 'jquery'], function (React) {
                         <header id="header">
                             <h1>Rego Search</h1>
 
-                            <form className="plateForm" onSubmit={this.handlePlateSearch}>
-                                <input type="text" ref="plate" id="placeNumber" placeholder="Plate #" onSubmit={this.handlePlateSearch} autoFocus={true} />
-                                <input type="submit" value="Search" />
-                            </form>
+                            <PlateForm onPlateSubmit={this.handlePlateSearch} />
                         </header>
                     </div>
+                );
+            }
+        });
+
+        var PlateForm = React.createClass({
+            handleSubmit: function(e) {
+                e.preventDefault();
+
+                var plateNumber = this.refs.plate.getDOMNode().value.trim();
+
+                if (!plateNumber) {
+                    return;
+                }
+
+                this.props.onPlateSubmit({ plate: plateNumber });
+                this.refs.plate.getDOMNode().value = '';
+            },
+            render: function () {
+                return (
+                    <form className="plateForm" onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Plate #" ref="plate" />
+                        <input type="submit" value="Plate" />
+                    </form>
                 );
             }
         });
