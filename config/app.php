17c1f758d8dfc604930e1ecb0c2048f19352d3b4
Dan Barrett <dan@theblahman.net>
2015-02-19 13:53:06 +0800

    Added Debugbar for... debugging...

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
 		'App\Providers\RouteServiceProvider',
 
         'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+        'Barryvdh\Debugbar\ServiceProvider',
 
 	],
 
@@ -194,6 +195,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
 
 	],
 
