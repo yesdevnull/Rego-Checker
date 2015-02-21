427c0717992e763e00e43830cb166db11269b69f
Dan Barrett <dan@theblahman.net>
2015-02-21 16:48:19 +0800

    Added routes and basic app layout

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -5,8 +5,50 @@ require(['react', 'router', 'jquery'], function (React, Router, $) {
     (function () {
         'use strict';
 
+        var DefaultRoute = Router.DefaultRoute;
+        var Link = Router.Link;
+        var Route = Router.Route;
+        var RouteHandler = Router.RouteHandler;
 
-        var Plates = React.createClass({
+        var PlatesApp = React.createClass({
+            render: function () {
+                return (
+                    <div>
+                        <header>
+                            <ul>
+                                <li><Link to="home">Home</Link></li>
+                                <li><Link to="search">Search</Link></li>
+                                <li><Link to="notify">Notify</Link></li>
+                            </ul>
+                        </header>
+
+                        <RouteHandler />
+                    </div>
+                );
+            }
+        });
+
+        var PlateHome = React.createClass({
+            render: function () {
+                return (
+                    <div>
+                        <h1>Home</h1>
+                    </div>
+                );
+            }
+        });
+
+        var PlateNotify = React.createClass({
+            render: function () {
+                return (
+                    <div>
+                        <h1>Notify</h1>
+                    </div>
+                )
+            }
+        });
+
+        var PlateSearch = React.createClass({
             handlePlateSearch: function (plate) {
                 this.setState({ response: 'Fetching...', type: 'info' }, function () {
                     $.ajax({
@@ -38,18 +80,16 @@ require(['react', 'router', 'jquery'], function (React, Router, $) {
             render: function () {
                 return (
                     <div>
-                        <header id="header">
-                            <h1>Rego Search</h1>
+                        <h1>Rego Search</h1>
 
-                            <PlateForm onPlateSubmit={this.handlePlateSearch} />
-                            <PlateResponse response={this.state.response} type={this.state.type} />
-                        </header>
+                        <PlateSearchForm onPlateSubmit={this.handlePlateSearch} />
+                        <PlateSearchResponse response={this.state.response} type={this.state.type} />
                     </div>
                 );
             }
         });
 
-        var PlateResponse = React.createClass({
+        var PlateSearchResponse = React.createClass({
             processResponseType: function(type, message) {
                 switch (type) {
                     case 'success' :
@@ -78,7 +118,7 @@ require(['react', 'router', 'jquery'], function (React, Router, $) {
             }
         });
 
-        var PlateForm = React.createClass({
+        var PlateSearchForm = React.createClass({
             handleSubmit: function (e) {
                 e.preventDefault();
 
@@ -101,13 +141,16 @@ require(['react', 'router', 'jquery'], function (React, Router, $) {
             }
         });
 
-        function render () {
-            React.render(
-                <Plates />,
-                document.getElementById('view')
-            );
-        }
+        var routes = (
+            <Route name="home" path="/" handler={PlatesApp}>
+                <Route name="search" handler={PlateSearch} />
+                <Route name="notify" handler={PlateNotify} />
+                <DefaultRoute handler={PlateHome} />
+            </Route>
+        );
 
-        render();
+        Router.run(routes, Router.HistoryLocation, function (Handler) {
+            React.render(<Handler />, document.getElementById('view'));
+        });
     })();
 });
