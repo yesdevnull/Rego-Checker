1528a41bdb0e760f42ef0cb9093084900a93a2db
Dan Barrett <dan@theblahman.net>
2015-02-25 11:45:55 +0800

    Removed anon function and added PlateSearch

--- resources/assets/js/app/Core.jsx
+++ resources/assets/js/app/Core.jsx
@@ -1,50 +1,45 @@
 define(['react', 'router', 'jsx!PlateHome', 'jsx!PlateSearch', 'jsx!PlateNotify', 'jsx!PlateFooter'], function(React, Router, PlateHome, PlateSearch, PlateNotify, PlateFooter) {
-    var app = app || {};
-    //var PlateHome = require('jsx!PlateHome');
+    'use strict';
 
-    (function() {
-        'use strict';
+    // var PureRenderMixin = React.addons.PureRenderMixin;
+    // mixins: [PureRenderMixin]
 
-        // var PureRenderMixin = React.addons.PureRenderMixin;
-        // mixins: [PureRenderMixin]
+    var DefaultRoute = Router.DefaultRoute;
+    var Link = Router.Link;
+    var Route = Router.Route;
+    var RouteHandler = Router.RouteHandler;
 
-        var DefaultRoute = Router.DefaultRoute;
-        var Link = Router.Link;
-        var Route = Router.Route;
-        var RouteHandler = Router.RouteHandler;
+    var PlateApp = React.createClass({
+        render: function () {
+            return (
+                <div>
+                    <header>
+                        <ul id="nav">
+                            <li><Link to="home">Home</Link></li>
+                            <li><Link to="search">Search</Link></li>
+                            <li><Link to="notify">Notify</Link></li>
+                        </ul>
+                    </header>
 
-        var PlateApp = React.createClass({
-            render: function () {
-                return (
-                    <div>
-                        <header>
-                            <ul id="nav">
-                                <li><Link to="home">Home</Link></li>
-                                <li><Link to="search">Search</Link></li>
-                                <li><Link to="notify">Notify</Link></li>
-                            </ul>
-                        </header>
+                    <section className="content">
+                        <RouteHandler />
+                    </section>
 
-                        <section className="content">
-                            <RouteHandler />
-                        </section>
+                    <PlateFooter />
+                </div>
+            );
+        }
+    });
 
-                        <PlateFooter />
-                    </div>
-                );
-            }
-        });
+    var routes = (
+        <Route name="home" path="/" handler={PlateApp}>
+            <Route name="search" handler={PlateSearch} />
+            <Route name="notify" handler={PlateNotify} />
+            <DefaultRoute handler={PlateHome} />
+        </Route>
+    );
 
-        var routes = (
-            <Route name="home" path="/" handler={PlateApp}>
-                <Route name="search" handler={PlateSearch} />
-                <Route name="notify" handler={PlateNotify} />
-                <DefaultRoute handler={PlateHome} />
-            </Route>
-        );
-
-        Router.run(routes, Router.HistoryLocation, function (Handler) {
-            React.render(<Handler />, document.getElementById('view'));
-        });
-    })();
+    Router.run(routes, Router.HistoryLocation, function (Handler) {
+        React.render(<Handler />, document.getElementById('view'));
+    });
 });
--- resources/assets/js/app/PlateSearch.jsx
+++ resources/assets/js/app/PlateSearch.jsx
@@ -1,13 +1,110 @@
-define(['react'], function (React) {
+define(['react', 'jquery'], function (React, $) {
     'use strict';
 
+    var app = app || {};
+
     var PlateSearch = React.createClass({
+        handlePlateSearch: function (plate) {
+            this.setState({ response: 'Fetching...', type: 'info' }, function () {
+                $.ajax({
+                    url: '/api/v1/plate',
+                    dataType: 'json',
+                    type: 'POST',
+                    beforeSend: function (xhr) {
+                        var token = $('meta[name="csrf_token"]').attr('content');
+
+                        if (token) {
+                            return xhr.setRequestHeader('X-XSRF-TOKEN', token);
+                        }
+                    },
+                    data: plate,
+                    success: function (data) {
+                        this.setState({ response: data.response.message, type: data.response.status });
+                    }.bind(this),
+                    error: function (xhr, status, err) {
+                        console.error('Error!');
+                        console.error(xhr);
+                        console.error(this.props.url, status, err.toString());
+                    }.bind(this)
+                });
+            });
+        },
+        getInitialState: function () {
+            return { response: '', type: '' };
+        },
         render: function () {
             return (
-                <p>Plate Search</p>
+                <div>
+                    <PlateSearchForm onPlateSubmit={this.handlePlateSearch} />
+                    <PlateSearchResponse response={this.state.response} type={this.state.type} />
+                </div>
             );
         }
     });
 
-    return PlateSearch;
+    var PlateSearchForm = React.createClass({
+        handleSubmit: function (e) {
+            e.preventDefault();
+
+            var plateNumber = this.refs.plate.getDOMNode().value.trim();
+
+            if (!plateNumber || (app.previousPlate == plateNumber)) {
+                return;
+            }
+
+            this.props.onPlateSubmit({ plate: plateNumber });
+            app.previousPlate = plateNumber;
+        },
+        render: function () {
+            return (
+                <form className="plateForm" onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Plate #" ref="plate" />
+                    <input type="submit" value="Search" />
+                    <p className="lawsuits"><strong>Note:</strong> I can't guarantee this data is correct.</p>
+                </form>
+            );
+        }
+    });
+
+    var PlateSearchResponse = React.createClass({
+        processResponseType: function(type, message) {
+            switch (type) {
+                case 'success' :
+                    return message;
+                    break;
+
+                case 'warning' :
+                    return 'Warning!  ' + message;
+                    break;
+
+                case 'error' :
+                    return 'Error: ' + message;
+                    break;
+
+                case 'info' :
+                    return 'Info: ' + message;
+                    break;
+            }
+        },
+        render: function () {
+            var niceResponse = this.processResponseType(this.props.type, this.props.response);
+
+            return (
+                <p>{niceResponse}</p>
+            );
+        }
+    });
+
+    var PlateSearchContainer = React.createClass({
+        shouldComponentUpdate: function () {
+            return false;
+        },
+        render: function () {
+            return (
+                <PlateSearch />
+            );
+        }
+    });
+
+    return PlateSearchContainer;
 });
