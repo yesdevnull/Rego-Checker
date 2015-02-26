bc599db45b8f7518c739db2d77b1f874231e23b7
Dan Barrett <dan@theblahman.net>
2015-02-26 16:39:40 +0800

    Basics for subscribe method

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,7 +1,38 @@
 <?php namespace App\Http\Controllers;
 
-class Notification extends Controller {
-    public function subscribe($email, $plate) {
+use Controller;
+use Validator;
+use Request;
+use App\Email;
+use App\Plate;
+use App\Exceptions\ApiErrorException;
 
+class Notification extends Controller {
+    public function subscribe(Request $request) {
+        $validator = Validator::make($request->all(), [
+            [
+                'email' => [
+                    'required',
+                    'email',
+                    'unique:email'
+                ]
+            ],
+            [
+                'plate' => [
+                    'required',
+                    'max:20'
+                ]
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            throw new ApiErrorException($validator->errors()->first(), 500);
+        } else {
+            $email = new Email;
+
+            $email->email = $request->input('email');
+        }
     }
+
+    // https://mailtrap.io/
 }
