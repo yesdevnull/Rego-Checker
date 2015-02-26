88a3fa6af17ef8f0d6da31105cf559229a1fda0c
Dan Barrett <dan@theblahman.net>
2015-02-26 20:58:34 +0800

    Display nice alert message on email subscribe

--- resources/assets/js/app/PlateNotify.jsx
+++ resources/assets/js/app/PlateNotify.jsx
@@ -1,4 +1,4 @@
-define(['react', 'jquery'], function (React, $) {
+define(['react', 'jquery', 'jsx!Alert'], function (React, $, Alert) {
     'use strict';
 
     var PlateNotify = React.createClass({
@@ -7,7 +7,7 @@ define(['react', 'jquery'], function (React, $) {
 
             return (
                 <div>
-                    <p>You can optionally be notified when the plate {plate} has 30 left until it expires by entering your email address below.</p>
+                    <p>You can optionally be notified when the plate {plate} has 30 days left until it expires by entering your email address below.</p>
                     <PlateNotifyForm plate={plate} />
                 </div>
             );
@@ -15,17 +15,69 @@ define(['react', 'jquery'], function (React, $) {
     });
 
     var PlateNotifyForm = React.createClass({
+        getInitialState: function () {
+            return {
+                response: '',
+                type: ''
+            }
+        },
         handleSubmit: function (e) {
             e.preventDefault();
 
+            var email = this.refs.email.getDOMNode().value.trim();
+            var plate = this.refs.plate.getDOMNode().value.trim();
 
+            this.setState({}, function() {
+                var that = this;
+
+                $.ajax({
+                    url: '/api/v1/subscribe',
+                    dataType: 'json',
+                    type: 'POST',
+                    beforeSend: function (xhr) {
+                        var token = $('meta[name="csrf_token"]').attr('content');
+
+                        if (token) {
+                            return xhr.setRequestHeader('X-XSRF-TOKEN', token);
+                        }
+                    },
+                    data: {
+                        email: email,
+                        plate: plate
+                    }
+                }).done(function (data) {
+                    console.log(data);
+                    that.setState({
+                        response: data.response.message,
+                        type: data.response.type
+                    });
+                }).fail(function (xhr, status, err) {
+                    // xhr
+                    console.log(xhr);
+                });
+            });
         },
         render: function () {
             return (
-                <form onSubmit={this.handleSubmit}>
-                    <input type="email" placeholder="Email Address" ref="email" />
-                    <input type="submit" value="Notify" />
-                </form>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="email" placeholder="Email Address" ref="email" />
+                        <input type="hidden" ref="plate" value={this.props.plate} />
+                        <input type="submit" value="Notify" />
+                    </form>
+                    <PlateNotifyResponse response={this.state.response} type={this.state.type} />
+                </div>
+            );
+        }
+    });
+
+    var PlateNotifyResponse = React.createClass({
+        render: function () {
+            console.log(this.props.type);
+            console.log(this.props.response);
+
+            return (
+                <Alert type={this.props.type} inputClasses={this.props.type}>{this.props.response}</Alert>
             );
         }
     });
