ad1f062ea9f8eec916b6e3952111343615a424ae
Dan Barrett <dan@theblahman.net>
2015-03-02 11:56:56 +0800

    Set global from email address

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'work@danbarrett.com.au', 'name' => 'Rego Checker'],
 
 	/*
 	|--------------------------------------------------------------------------
