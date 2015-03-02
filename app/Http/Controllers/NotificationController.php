c308021c9d88a93f401796467519038c6cbe3eb1
Dan Barrett <dan@theblahman.net>
2015-03-02 09:31:07 +0800

    Fixed up email/plate logic

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -17,7 +17,7 @@ use App\Exceptions\ApiErrorException;
 class NotificationController extends Controller {
     /**
      * @param Request $request
-     * @return array
+     * @return \Symfony\Component\HttpFoundation\Response
      * @throws ApiErrorException
      */
     public function subscribe(Request $request) {
@@ -41,62 +41,72 @@ class NotificationController extends Controller {
                 $query->where('plate', '=', $request->input('plate'));
             })->get();
 
-            Debugbar::info('Email/Plate whereHas Count: ' . count($emailPlatesMatch));
-
             if (count($emailPlatesMatch) > 0) {
-                // Filter through the emails and pluck out all emails that don't match the input
+                // Filter through the emails and pluck out all emails that match the input
                 $sameEmail = $emailPlatesMatch->filter(function($email) use ($request) {
                     if ($email->email === $request->input('email')) {
                         return $email;
                     }
                 });
 
-                Debugbar::info('sameEmail: ' . count($sameEmail));
+                Log::info('sameEmail: ' . count($sameEmail));
+                Log::info($sameEmail);
 
                 // If the email address exists in that list, they have already subscribed to this plate/email alert
                 if (count($sameEmail) > 0) {
                     throw new ApiErrorException('This email address and plate are already subscribed.', 500);
                 }
-            } else {
-                $email = Email::where('email', '=', $request->input('email'))->first();
-
-                Debugbar::info('Email exists: ' . count($email));
-                Debugbar::info($email);
-
-                if (count($email) > 0) {
-                    Debugbar::info('Email exists, assign further work to first email');
-                } else {
-                    Debugbar::info('Email does not exist, let\'s create the model');
-                    $email = new Email;
-
-                    $email->email = $request->input('email');
-                    $email->enabled = true;
-                    // Just need a random string for the email confirmation token
-                    $email->token = Crypt::encrypt($request->input('email'));
-
-                    // Save the Email Model
-                    $email->save();
-                }
             }
 
-            // If we get this far, the plate doesn't exist, so let's create it
-            $plate = new Plate;
+            // Email address hasn't been used in association with the supplied plate
+            $email = Email::where('email', '=', $request->input('email'))->first();
 
-            $plate->state = 'WA';
-            $plate->plate = $request->input('plate');
+            Log::info('Email exists: ' . count($email));
+            Log::info($email);
 
-            // Save the Plate Model
-            $plate->save();
+            if (count($email) > 0) {
+                Log::info('Email exists, assign further work to first email');
+            } else {
+                Log::info('Email does not exist, let\'s create the model');
+                $email = new Email;
+
+                $email->email = $request->input('email');
+                $email->enabled = true;
+                // Just need a random string for the email confirmation token
+                $email->token = Crypt::encrypt($request->input('email'));
+
+                // Save the Email Model
+                $email->save();
+            }
+
+            // See if plate is already in database
+            $plates = Plate::where('state', '=', 'WA', 'and')->where('plate', '=', $request->input('plate'))->get();
+
+            if (count($plates) > 0) {
+                // Plate is already in database, use it from now on
+                Log::info('Plate exists, using result from Plate::where query');
+                $plate = $plates[0];
+            } else {
+                // If we get this far, the plate doesn't exist, so let's create it
+                $plate = new Plate;
+
+                $plate->state = 'WA';
+                $plate->plate = $request->input('plate');
+
+                // Save the Plate Model
+                $plate->save();
+                Log::info('Plate created and saved');
+            }
 
             // Link Plate to Email
             $email->plates()->save($plate);
 
             Log::info('Saved email and plate to database');
 
-            return [
+            return response()->json([
                 'type' => 'success',
                 'message' => 'Successfully subscribed to notifications.  Please check your inbox to confirm your email address'
-            ];
+            ]);
         }
     }
 
