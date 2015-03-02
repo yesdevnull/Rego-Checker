37a39190f3dd7b972628a908ed852eba54dc2339
Dan Barrett <dan@theblahman.net>
2015-03-02 17:59:38 +0800

    Basic handling and processing for plates

--- app/Http/Controllers/PlateCrawlerController.php
+++ app/Http/Controllers/PlateCrawlerController.php
@@ -25,10 +25,6 @@ class PlateCrawlerController extends Controller {
         $plates->each(function($plate) use ($that) {
             Log::info(sprintf('handling dispatch for plate %s', $plate->plate));
 
-//            Bus::dispatch(
-//                new CrawlPlates($plate)
-//            );
-
             $that->dispatch(
                 new CrawlPlates($plate)
             );
@@ -44,16 +40,41 @@ class PlateCrawlerController extends Controller {
             case 'WA' :
                 $result = $checkerController->_waRegoCheck($plate->plate, false);
 
-                //dd($result);
                 if ($result['status'] == 'success') {
                     preg_match('/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/i', $result['message'], $date);
 
                     $plate->status_text = $date[0];
                     $plate->status = $plate->status_list[$result['status']];
-                    $plate->last_searched = Carbon::now()->toDateTimeString();
 
-                    $plate->save();
+                    Log::info(sprintf('In-date status for plate %s', $plate->plate));
+                } elseif ($result['status'] == 'warning' && preg_match('#unregistered#i', $result['message'])) {
+                    $plate->status_text = $result['message'];
+                    $plate->status = $plate->status_list['unregistered'];
+
+                    Log::info(sprintf('Unregistered status for plate %s', $plate->plate));
+                } elseif ($result['status' == 'error']) {
+                    if (preg_match('#unknown#i', $result['message'])) {
+                        $plate->status_text = $result['message'];
+                        $plate->status = $plate->status_list[0];
+
+                        Log::info(sprintf('Unknown status for plate %s', $plate->plate));
+                    } elseif (preg_match('#unable to locate#i', $result['message'])) {
+                        if ($plate->status == 99) {
+                            // Plate was invalid yesterday, and is again invalid after this search - delete time
+
+                            Log::info(sprintf('Plate %s failed 2 days in a row, deleting', $plate->plate));
+
+                            $plate->delete();
+
+                            return true;
+                        }
+                    }
                 }
+
+                $plate->last_searched = Carbon::now()->toDateTimeString();
+                $plate->save();
+
+                Log::info(sprintf('Finished crawling plate %s', $plate->plate));
             break;
         }
     }
